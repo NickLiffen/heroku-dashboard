0116c818ac7495fe20063fc74d2dd0e7f090d071
NickLiffen <nickliffen@hotmail.com>
2016-07-11 14:44:10 +0100

    Fixing Search:

--- public/js/index.js
+++ public/js/index.js
@@ -37,22 +37,22 @@
                 );
             });
             $('#ClassList table').append(tableContent);
+
+            var $rows = $('#herokuAppTable tbody tr');
+            console.log($rows);
+            $('#search').keyup(function() {
+
+                var val = '^(?=.*\\b' + $.trim($(this).val()).split(/\s+/).join('\\b)(?=.*\\b') + ').*$',
+                    reg = RegExp(val, 'i'),
+                    text;
+
+                $rows.show().filter(function() {
+                    text = $(this).text().replace(/\s+/g, ' ');
+                    return !reg.test(text);
+                }).hide();
+            });
         });
 
-
-        var $rows = $('#herokuAppTable tbody tr');
-        console.log($rows);
-        $('#search').keyup(function() {
-
-            var val = '^(?=.*\\b' + $.trim($(this).val()).split(/\s+/).join('\\b)(?=.*\\b') + ').*$',
-                reg = RegExp(val, 'i'),
-                text;
-
-            $rows.show().filter(function() {
-                text = $(this).text().replace(/\s+/g, ' ');
-                return !reg.test(text);
-            }).hide();
-        });
 
     }); // end of document ready
 })(jQuery); // end of jQuery name space
