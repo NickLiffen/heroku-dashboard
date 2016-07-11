33972e655d0718eaf6f46078987980e4e79a7366
NickLiffen <nickliffen@hotmail.com>
2016-07-11 13:07:33 +0100

    Getting all app names

--- public/js/index.js
+++ public/js/index.js
@@ -1,24 +1,35 @@
 "use strict";
 
-(function($){
-  $(function(){
+(function($) {
+    $(function() {
 
-    $(".button-collapse").sideNav();
+        $(".button-collapse").sideNav();
 
-    $.ajax({
-    type: 'GET',
-    url: '/totalHerokuApps',
-  }).done(function(response){
-    $('#totalHerokuApps').html(response.length);
-  });
+        $.ajax({
+            type: 'GET',
+            url: '/totalHerokuApps',
+        }).done(function(response) {
+            $('#totalHerokuApps').html(response.length);
+        });
 
-  $.ajax({
-  type: 'GET',
-  url: '/totalHerokuAddOns',
-}).done(function(response){
-  $('#totalHerokuAddOns').html(response.length);
-});
+        $.ajax({
+            type: 'GET',
+            url: '/totalHerokuAddOns',
+        }).done(function(response) {
+            $('#totalHerokuAddOns').html(response.length);
+        });
+
+        $.ajax({
+            type: 'GET',
+            url: '/totalHerokuApps',
+        }).done(function(response) {
+            let arrayLength = response.length;
+            for (var i = 0; i < arrayLength; i++) {
+                console.log(myStringArray[i]);
+
+            }
+        });
 
 
-  }); // end of document ready
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
