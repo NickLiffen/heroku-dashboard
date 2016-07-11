946b3fa5d8a17194f822307b4256c49dad721b23
NickLiffen <nickliffen@hotmail.com>
2016-07-11 11:49:21 +0100

    Adding Heroku API

--- public/js/index.js
+++ public/js/index.js
@@ -4,5 +4,16 @@
   $(function(){
 
     $(".button-collapse").sideNav();
+
+    $.ajax({
+    type: 'GET',
+    url: '/totalHerokuApps',
+    dataType: 'JSON'
+  }).done(function(response){
+    console.log(response);
+
+  });
+
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
