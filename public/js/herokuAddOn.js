0fafbd804f9e4ebacfcac279cbd712cc3cecfb24
Nick Liffen <UP653591@myport.ac.uk>
2016-07-12 19:56:57 +0100

    Moved charts to home page

--- public/js/herokuAddOn.js
+++ public/js/herokuAddOn.js
@@ -5,94 +5,15 @@
 
         $(".button-collapse").sideNav();
 
-        //This function inputs one array - then outputs two different arrays. Array 1: The number of times an element appears in the array. Array 2. Element Name.
-        //Exmaple. Input: [Nick, Bob, Nick] Ouput: [Nick, Bob] [2, 1]
-        function order(arr) {
-            var a = [],
-                b = [],
-                prev;
-
-            arr.sort();
-            for (var i = 0; i < arr.length; i++) {
-                if (arr[i] !== prev) {
-                    a.push(arr[i]);
-                    b.push(1);
-                } else {
-                    b[b.length - 1]++;
-                }
-                prev = arr[i];
-            }
-
-            return [a, b];
-        }
-        //This function acts as a callback function for creating a bar chart chart. Saves having to write all the code again multiple times.
-        function barChartCreator(DOMLocation, labels, data, label, title){
-          var createdChart = new Chart(DOMLocation, {
-              type: 'bar',
-              data: {
-                  labels: labels,
-                  datasets: [{
-                      label: '# Heorku AddOns',
-                      data: data,
-                      backgroundColor: [
-                          'rgba(255, 99, 132, 0.2)',
-                          'rgba(54, 162, 235, 0.2)',
-                          'rgba(255, 206, 86, 0.2)',
-                          'rgba(75, 192, 192, 0.2)',
-                          'rgba(153, 102, 255, 0.2)',
-                          'rgba(255, 159, 64, 0.2)'
-                      ],
-                      borderColor: [
-                          'rgba(255,99,132,1)',
-                          'rgba(54, 162, 235, 1)',
-                          'rgba(255, 206, 86, 1)',
-                          'rgba(75, 192, 192, 1)',
-                          'rgba(153, 102, 255, 1)',
-                          'rgba(255, 159, 64, 1)'
-                      ],
-                      borderWidth: 1
-                  }]
-              },
-              options: {
-                  scales: {
-                      yAxes: [{
-                          ticks: {
-                              beginAtZero:true
-                          }
-                      }]
-                  },
-                  title: {
-                    display: true,
-                      text: 'Number of AddOns Per Applications'
-                      }
-              }
-          });
-          return createdChart;
-        }
-
-
         $.ajax({
             type: 'GET',
             url: '/totalHerokuAddOns',
         }).done(function(response) {
 
-
-            var applicationName = [];
-            var addOnName = [];
-
             var tableContent;
             tableContent = $('<tbody></tbody>');
 
             $.each(response, function() {
-              //This strips the response to include only the string before the -
-              //addOnName.push(this.name.splt('-')[0])
-
-              var tempAddOn = this.name;
-              var strippedAddOn = tempAddOn.split('-')[0];
-              addOnName.push(strippedAddOn);
-
-              applicationName.push(this.app.name);
-
                 tableContent.append(
                     `<tr>
                       <td>${this.name}</td>
@@ -103,23 +24,6 @@
             });
 
             $('#HerokuAddOns table').append(tableContent);
-
-
-
-
-            //Data for the Chart JS
-            var result = order(applicationName);
-            var herokuAppName = result[0];
-            var NoOfAddOns    = result[1];
-            var chartOne = document.getElementById("addOnsPerApplication");
-            barChartCreator(chartOne, herokuAppName, NoOfAddOns, '# Heorku AddOns', 'Number of AddOns Per Applications');
-
-            var resultNew = order(addOnName);
-            var herokuAddOnName = resultNew[0];
-            var totalNoOfAddOns    = resultNew[1];
-            var chartTwo = document.getElementById("totalOfEachAddOn");
-            barChartCreator(chartTwo, herokuAddOnName, totalNoOfAddOns, '# Amount of Addons ', 'Total amount of each Add On in Heroku');
-
 
             var $rows = $('#herokuAddOnsTable tbody tr');
 
@@ -136,7 +40,6 @@
 
             });
         });
-
 
     }); // end of document ready
 })(jQuery); // end of jQuery name space
